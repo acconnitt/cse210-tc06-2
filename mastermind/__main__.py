a44358e16e4cb5751da270ad88e36c6d2cbbeaa4
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-31 21:44:24 -0400

    ...
    ...

--- mastermind/__main__.py
+++ mastermind/__main__.py
@@ -1,4 +1,4 @@
 from game.director import Director
 
-director = Director()
-director.start_game()
+director = Director() #Calls director from Director class
+director.start_game() #Calls start_game from Director class
--- mastermind/game/director.py
+++ mastermind/game/director.py
@@ -9,19 +9,22 @@ from game.console import Console
 class Director:
 
     def __init__(self):
-        self.roster = Roster()
-        self.console = Console()
-        self.keep_playing = True
-        self.win = False
+        """ Constructor method. 
+            Object is initialized
+        """
+        self.roster = Roster()  # Calls Roster class
+        self.console = Console()  # Calls Console class
+        self.keep_playing = True  # Keep playing while true
+        self.win = False  # Starts as false while player hasn't won
 
     def start_game(self):
-        self.prepare_game()
-        self.roster.next_player()
-        while self.keep_playing:
+        self.prepare_game()  # Calls prepare_game method
+        self.roster.next_player()  # Calls next_player method from Roster class
+        while self.keep_playing:  # keep_playong while is true
             print()
-            self._get_inputs()
-            self._do_updates()
-            self.roster.next_player()
+            self._get_inputs()  # Calls get_inputs method
+            self._do_updates()  # Calls do_updates
+            self.roster.next_player()  # Calls next_player method from Roster class
 
     def _get_inputs(self):
         player = self.roster.get_current()
@@ -36,10 +39,12 @@ class Director:
             guess = input("What is your guess? ")
             for i in guess:
                 if i.isalpha():
-                    print("Your guess was invalid because it contained letters, please try again. ")
+                    print(
+                        "Your guess was invalid because it contained letters, please try again. ")
                     break
             if len(guess) != 4:
-                print("Your guess was invalid because it wasn't 4 characters long, please try again.")
+                print(
+                    "Your guess was invalid because it wasn't 4 characters long, please try again.")
             else:
                 valid_guess = True
 
@@ -59,10 +64,10 @@ class Director:
             Args:
                 self (Director): An instance of Director.
         """
-        code = Code()
-        the_code = code.get_code()
+        code = Code()  # Calls the Code class
+        the_code = code.get_code()  # Calls the get_code method from Code class
         for n in range(2):
-            name = input(f"Enter a name for player {n + 1}: ")
+            name = input(f"Enter a name for player {n + 1}: ") # Players name input
             guess = Guess(the_code)
             player = Player(name, guess, code)
             self.roster.add_player(player)
